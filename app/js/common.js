21e25a74e8a1905af29c2c9735e7dd10316f4108
viktoriazaharova <zaharova181114@gmail.com>
2020-10-16 01:35:48 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,62 @@
+$(".form").submit(function () {
+    $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()
+    }).done(function () {
+        $(this).find("input").val("");
+
+        $('.modal__div').css('display', 'none').animate({
+            opacity: 0,
+            top: '45%'
+        });
+
+        $('#thanks__modal').css('display', 'flex')
+            .animate({
+                opacity: 1,
+                top: '50%'
+            }, 200);
+
+        $(".form").trigger("reset");
+    });
+    return false;
+});
+
+// модальные окна (несколько)
+$(document).ready(function () {
+    var overlay = $('.overlay');
+    var open_modal = $('.open_modal');
+    var close = $('.modal__close, .overlay');
+    var modal = $('.modal__div');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        var div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.click(function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
+
+
+$("[name=phone]").mask("+7 (999) 999 99 99");
---
+++ app/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "info@steeltube.kz";
+$sitename = "Steeltube.kz";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
